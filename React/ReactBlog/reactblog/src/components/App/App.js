700aa31f931e435e856dc44b341c597a500d6bac
PredragBelos <predragbelos@gmail.com>
2021-08-12 12:24:29 +0200

    Prepare project structure for start

---
+++ React/ReactBlog/reactblog/src/components/App/App.js
@@ -0,0 +1,12 @@
+import { createHD } from '../Header/header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {createHD()}
+    </div>
+  );
+}
+
+export default App;
---
+++ React/ReactBlog/reactblog/src/components/Header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export let createHD = () => {
+    return(
+        <div>
+            <p>Proba</p>
+        </div>
+    );
+}
+
+
+
+
+
