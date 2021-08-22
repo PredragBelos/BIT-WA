5b3a8a16c9ed62084e969af74c8cb7a1d334f2e3
PredragBelos <predragbelos@gmail.com>
2021-08-22 13:50:43 +0200

    Feature2 -create mainSection component

--- React/BitPeople/src/components/App/App.js
+++ React/BitPeople/src/components/App/App.js
@@ -1,11 +1,13 @@
 import { Footer } from '../Footer/footer';
 import { Header } from '../Header/header';
+import { MainSection } from '../Main section/mainSection';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <MainSection/>
       <Footer/>
     </div>
   );
---
+++ React/BitPeople/src/components/Main section/mainSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './scss/mainSection.css';
+
+/*Function for create HTML elements for rendering*/
+export const MainSection = () => {
+    
+    return(
+        <section className="container-flow mainSection">
+            <div className="container mainSectionContainer">
+                <div className="row mainSectionRow">
+                    
+                </div>
+            </div>
+        </section>
+    );
+}
