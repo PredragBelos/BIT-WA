45e471a485b92e5500ec0144b4ff52452f60cf1b
PredragBelos <predragbelos@gmail.com>
2021-08-27 09:55:00 +0200

    Task3, Task4, Task5 - Create basic folder structure and basic components

--- React/bitblog/src/components/App/App.js
+++ React/bitblog/src/components/App/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Redirect, Route} from 'react-router-dom';
+import { Home } from '../Home/home';
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Route path="/home" component={Home}/>
+      
+      <Redirect from="/" to="/home"/>
     </div>
   );
 }
